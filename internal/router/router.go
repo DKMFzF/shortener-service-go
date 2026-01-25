e6cf80a03e18ad7b94cc67fe22d8411c508d805f
DKMFzF <flim.win@yandex.ru>
2026-01-25 14:27:55 +0700

    refactor(router): added di in router

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,21 +4,34 @@ import (
 	"shortener/internal/handler/getUrl"
 	"shortener/internal/handler/pong"
 	"shortener/internal/handler/short"
+	"shortener/internal/logger"
 
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func SetupRouter(router *gin.Engine) *gin.RouterGroup {
-	apiGroup := router.Group("/api/v1")
+type BaseController struct {
+	Router *gin.Engine
+	Logger *logger.Logger
+}
+
+func New(driver *gin.Engine, logger *logger.Logger) *BaseController {
+	return &BaseController{
+		Router: driver,
+		Logger: logger,
+	}
+}
+
+func (c *BaseController) SetupRouter() *gin.RouterGroup {
+	apiGroup := c.Router.Group("/api/v1")
 	{
 		apiGroup.GET("/:id", getUrl.GetUrlHandler)
 		apiGroup.POST("/", short.ShortHandler)
 	}
 
-	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	router.GET("/ping", pong.PongHandler)
+	c.Router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	c.Router.GET("/ping", pong.PongHandler)
 
 	return apiGroup
 }
